fa04b69be8061d884adf54846f4e12deb9a05f29
mlopshayyan <hayyan16161@gmail.com>
2024-07-25 15:52:44 +0530

    error

--- mlartifacts/547525848799594067/be5a26e493254f2f8d5e01e42862103c/artifacts/iris-dt.py
+++ mlartifacts/547525848799594067/be5a26e493254f2f8d5e01e42862103c/artifacts/iris-dt.py
@@ -17,7 +17,7 @@ data = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(
     data.data, data.target, test_size=0.2, random_state=42)
 
-max_depth=10
+max_depth=15
 
 mlflow.set_experiment('iris-dt')
 # Log model hyperparameters and metrics to the MLflow server
